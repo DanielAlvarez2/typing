51dd60dcf7cced720085fbb220d5aa4d1958fe80
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-06-07 12:39:14 -0400

    add sound effects

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import { useState } from 'react'
 
 const targets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-
+const wrong = new Audio('wrong.mp3')
+const correct = new Audio('correct.mp3')
 
 function App() {
 
   function checkAnswer(){
     if(document.querySelector('#right').value == document.querySelector('#left').textContent) {
       document.querySelector('#right').style.backgroundColor = 'green' 
+      correct.play()
       reset()
     }else{
-      document.querySelector('#right').style.backgroundColor = 'red' 
+      document.querySelector('#right').style.backgroundColor = 'red'
+      wrong.play() 
       reset()
     }
   }
@@ -27,7 +30,7 @@ function App() {
         newTarget = targets[Math.floor(Math.random()*targets.length)]
       }while(newTarget == document.querySelector('#left').value)
       setTarget(newTarget)
-    },1000)
+    },1200)
   }
   
   const [target, setTarget] = useState(targets[Math.floor(Math.random()*targets.length)])
@@ -35,7 +38,7 @@ function App() {
   return (
     <main>
      <h1 id='left'>{target}</h1>
-     <input autoFocus id='right' onKeyUp={checkAnswer} />
+     <input autoFocus id='right' maxlength='1' onKeyUp={checkAnswer} />
     </main>
   )
 }
