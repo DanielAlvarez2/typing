1bc1c9f9c040a0f35bb5becef3a9a18b1d19d20f
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-06-07 09:07:56 -0400

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,29 @@ import { useState } from 'react'
 
 const targets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-function checkAnswer(){
-  if(document.querySelector('#right').value == document.querySelector('#left').textContent) {
-    document.querySelector('#right').style.backgroundColor = 'green' 
-  }else{
-    document.querySelector('#right').style.backgroundColor = 'red' 
-  }
-}
+
 
 function App() {
+
+  function checkAnswer(){
+    if(document.querySelector('#right').value == document.querySelector('#left').textContent) {
+      document.querySelector('#right').style.backgroundColor = 'green' 
+      reset()
+    }else{
+      document.querySelector('#right').style.backgroundColor = 'red' 
+      reset()
+    }
+  }
+  
+
+  function reset(){
+    setTimeout(()=>{
+      document.querySelector('#right').value = ''
+      document.querySelector('#right').style.backgroundColor = 'blue'
+      setTarget(targets[Math.floor(Math.random()*targets.length)])
+    },1000)
+  }
+  
   const [target, setTarget] = useState(targets[Math.floor(Math.random()*targets.length)])
 
   return (
